cdcc48d3352fee82d5cb0b31c249419af365abc3
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-20 18:09:48 -0600

    Refactor App component to use react-router-dom
    This commit refactors the App component in order to use the react-router-dom library for routing. Sets up the routes for the HomePage and BlogPost components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import BlogPost from './pages/BlogPost';
+import { React, useState } from 'react';
 
 function App() {
+  const [getBlogContent, setGetBlogContent] = useState([]);
+  const getData = (blog) => {
+    setGetBlogContent(blog);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className='app-container'>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<HomePage data={getData} />} />
+            <Route
+              path='/blog/:id'
+              element={<BlogPost content={getBlogContent} />}
+            />
+          </Routes>
+        </div>
+      </div>
+    </div >
   );
 }
-
 export default App;
