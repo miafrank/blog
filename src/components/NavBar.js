15fa0f7e090b1560e25e737e5310a3b709130144
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-22 17:05:56 -0600

    Refactor NavBar component to use Flowbite-React

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,63 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../index.css';
-import { NavLink } from 'react-router-dom';
-import { blogList } from '../config/Api';
-import SearchBar from './SearchBar';
+// import { NavLink } from 'react-router-dom';
+// import Link from 'next/link';
+import { Navbar, NavbarCollapse, NavbarLink, NavbarToggle } from 'flowbite-react'
 
-const NavBar = () => {
-    const [blogs, setBlogs] = useState([]);
-    const [searchKey, setSearchKey] = useState('');
-
-    const handleSearchBar = (e) => {
-        e.preventDefault();
-        handleSearchResults();
-    };
-
-    const handleSearchResults = () => {
-        const filtered = blogs.filter((blog) => {
-            return blog.tags[0].name.toLowerCase().includes(searchKey.toLowerCase());
-        });
-        setBlogs(filtered);
-    };
-
-    const handleClearSearch = () => {
-        blogList().then((res) => {
-            setBlogs(res);
-        });
-        setSearchKey('');
-    };
-
+const NavBarHome = () => {
     return (
-        <nav className="navbar">
-            <div className="navbar-container">
-                <div className="navbar-elements-container">
-                    <ul>
-                        <li>
-                            <NavLink to="/about">About</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/blog">Blog</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/projects">Projects</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/contact">Contact</NavLink>
-                        </li>
-                        <li>
-                            {/* Search Bar */}
-                            <SearchBar
-                                value={searchKey}
-                                clearSearch={handleClearSearch}
-                                formSubmit={handleSearchBar}
-                                handleSearchKey={(e) => setSearchKey(e.target.value)}
-                            />
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <div>
+            <Navbar fluid rounded>
+                <NavbarToggle />
+                <NavbarCollapse>
+                    <NavbarLink href="/about">About</NavbarLink>
+                    <NavbarLink href="/blog">Blog</NavbarLink>
+                    <NavbarLink href="/projects">Projects</NavbarLink>
+                </NavbarCollapse>
+            </Navbar>
+        </div>
+
     )
 }
 
-export default NavBar;
+export default NavBarHome;
