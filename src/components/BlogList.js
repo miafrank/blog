9c78fccb57d3c7191c1a31c4e9582973194a980c
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-23 18:51:36 -0600

    Use tailwind dl in blog list.
    Added filler blog posts for testing styling

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -5,9 +5,21 @@ import '../index.css';
 const BlogList = ({ blogs, content }) => {
     return (
         <div className='blogList-wrap'>
-            {blogs.map((blog) => (
-                <BlogItem blog={blog} content={content} />
-            ))}
+            <dl class="max-w-md text-black-900 divide-y divide-black-200 dark:text-black dark:divide-black-700">
+                <div class="flex flex-col pb-3">
+                    <dt class="mb-3 text-gray-500 md:text-lg dark:text-gray-400">{
+                        blogs.map((blog) => (
+                            <BlogItem blog={blog} content={content} />
+                        ))}</dt>
+                </div>
+
+                <div class="flex flex-col pb-3">
+                    <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">{
+                        blogs.map((blog) => (
+                            <BlogItem blog={blog} content={content} />
+                        ))}</dt>
+                </div>
+            </dl>
         </div>
     );
 };
