caf240cd126fe0f69e8d858061cbf390a6bea22b
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-23 19:54:04 -0600

    Remove duplicate Header component and reposition it

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -35,12 +35,12 @@ const HomePage = ({ data }) => {
 
     return (
         <div>
-            <Header />
             <NavBar
                 value={searchKey}
                 clearSearch={handleClearSearch}
                 formSubmit={handleSearchBar}
                 handleSearchKey={(e) => setSearchKey(e.target.value)} />
+            <Header />
             <div className='blogItem-wrap'>
                 <p class="mb-3 text-left leading-loose text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:me-3 first-letter:float-start">
                     About Me:
