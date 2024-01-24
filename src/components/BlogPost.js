a666f568b0939f0406e8fbd969421216bc02b8d0
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-23 18:52:04 -0600

    Refactor BlogPost component
    Use tailwind

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,25 +5,20 @@ import '../index.css';
 import { Link } from 'react-router-dom';
 
 const BlogPost = ({ content }) => {
+    console.log({ content })
     return (
         <>
-            <Link className='blogPost-goBack' to='/'>
+            <Link className='blogPost-goBack' to='/blogs'>
                 <span> &#8592;</span> <span>Go Back</span>
             </Link>
             {content ? (
                 <div className='blog-wrap'>
                     <header>
                         <p className='blog-date'>Published {content.created}</p>
-                        <h1>{content.title}</h1>
-                        <div className='blog-subCategory'>
-
-                            <div>
-                                <Chip label={content.tags[0].name} />
-                            </div>
-
-                        </div>
+                        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">{content.title}</h1>
                     </header>
-                    <div className='blog-content' dangerouslySetInnerHTML={{ __html: content.body }}></div>
+                    <p class="mb-3 whitespace-pre-line text-sm text-gray-900 dark:text-white"
+                        dangerouslySetInnerHTML={{ __html: content.body }}></p>
                 </div>
             ) : (
                 <EmptyList />
