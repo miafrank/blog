38c978739e84ee48428f46b68c2f649353dad874
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-30 15:05:33 -0600

    Refactor BlogList component styling

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -4,17 +4,17 @@ import '../index.css';
 
 const BlogList = ({ blogs, content }) => {
     return (
-        <div className='blogList-wrap'>
-            <dl class="max-w-md text-black-900 divide-y divide-black-200 dark:text-black dark:divide-black-700">
-                <div class="flex flex-col pb-3">
-                    <dt class="mb-3 text-gray-500 md:text-lg dark:text-gray-400">{
+        <div class='flex justify-center'>
+            <dl class="max-w-md text-gray-700 dark:text-black">
+                <div class="flex flex-col pt-3">
+                    <dt class="mb-3 text-gray-500 dark:text-gray-400">{
                         blogs.map((blog) => (
                             <BlogItem blog={blog} content={content} />
                         ))}</dt>
                 </div>
 
-                <div class="flex flex-col pb-3">
-                    <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">{
+                <div class="flex flex-col pt-3">
+                    <dt class="mb-3 text-gray-500 md:text-lg dark:text-gray-400">{
                         blogs.map((blog) => (
                             <BlogItem blog={blog} content={content} />
                         ))}</dt>
