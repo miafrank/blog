ab92a3980b384eb862b2a1e8df59acbb72c37616
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-02-23 16:45:34 -0600

    Format Api.js

--- src/config/Api.js
+++ src/config/Api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const read_token = process.env.REACT_APP_READ_TOKEN;
 
 export const blogList = async () => {
-    const url = `
+  const url = `
   https://api.buttercms.com/v2/posts?auth_token=${read_token}`;
-    return axios.get(url).then((res) => {
-        return res.data.data;
-    });
+  return axios.get(url).then((res) => {
+    return res.data.data;
+  });
 };
