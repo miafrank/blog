c84df5b63373d749865367fc363127e939be80e7
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-02-24 17:30:50 -0600

    Refactor Footer component layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,34 @@
+'use client';
+
 import React from 'react';
 import '../index.css';
 import { Footer } from 'flowbite-react';
 import { BsGithub, BsLinkedin, BsMailbox, BsPhone } from 'react-icons/bs';
 
 const HomeFooter = () => (
-    <Footer container>
+    <footer class="bg-white shadow  border-gray-200 dark:bg-gray-900">
         <div className="w-full">
-            <div className="w-full sm:flex sm:items-center sm:justify-between">
-                <Footer.Copyright href="#" by="Mia Frank" year={2024} />
-                <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-                    <Footer.Icon href="https://github.com/miafrank/" icon={BsGithub} />
-                    <Footer.Icon href="https://www.linkedin.com/in/mia-frank/" icon={BsLinkedin} />
-                    <Footer.Icon href="tel:314-000-0000" icon={BsPhone} />
-                    <Footer.Icon href="mailto: mia.jo.frank@gmail.com" icon={BsMailbox} />
+            <div class="max-w-screen-xl mx-auto p-4 md:py-8">
+                <div class="flex items-center justify-between">
+                    <Footer.Copyright href="https://www.linkedin.com/in/mia-frank/" by="Mia Frank" year={2024} />
+                    <ul class="flex flex-wrap items-center space-x-4 mb-6 text-sm font-medium text-left text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <li>
+                            <Footer.Icon href="https://github.com/miafrank/" icon={BsGithub} />
+                        </li>
+                        <li>
+                            <Footer.Icon href="https://www.linkedin.com/in/mia-frank/" icon={BsLinkedin} />
+                        </li>
+                        <li>
+                            <Footer.Icon href="tel:314-000-0000" icon={BsPhone} />
+                        </li>
+                        <li>
+                            <Footer.Icon href="mailto: mia.jo.frank@gmail.com" icon={BsMailbox} />
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
-    </Footer>
+    </footer>
 );
 
 export default HomeFooter;
