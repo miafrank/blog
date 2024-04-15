7e4c556d001eaab8e165743e28fae5436ce973a3
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-04-15 17:50:08 -0500

    Add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import '../index.css'
+
+const Card = ({ repoName, repoLink, repoDescription, repoImg }) => {
+  return (
+        <div class='flex justify-evenly pt-16'>
+            <a href={repoLink} class="block max-w-lg p-6 bg-white border border-hot-pink rounded-md shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{repoName}</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">{repoDescription}</p>
+            </a>
+        </div>
+  )
+}
+
+export default Card
