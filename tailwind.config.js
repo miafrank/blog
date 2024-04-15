2d44fba75329f7675aaa2be03b47aff28d60be9d
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-04-15 16:27:02 -0500

    Update tailwind.config.js with new background images and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
+    './src/**/*.{html,js}',
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
-    extend: {},
-    container: {
-      center: true,
-    },
     backgroundImage: {
-      'gradient': "url('../src/images/gradient.svg')",
-      'header': "url('../src/images/Home.png')",
+      // gradient: "url('../src/images/gradient.svg')",
+      gradient: "url('../src/images/pattern.svg')",
+      header: "url('../src/images/Home.png')"
     },
+    colors: {
+      aqua: '#58E4E0',
+      'tiffany-blue': '#7ED8E0',
+      'royal-blue': '#294EA0',
+      'hot-pink': '#F766A0'
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+      serif: ['Merriweather', 'serif']
+    },
+    container: {
+      center: true
+    },
+    extend: {}
   },
   plugins: [
-    require('flowbite/plugin'),
-  ],
+    require('flowbite/plugin')
+  ]
 }
