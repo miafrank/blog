9df913cdaf9c430366fe65aea02cb1e2b5a44df8
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-04-22 14:05:18 -0500

    Add card images and update ProjectsPage component

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,41 +1,58 @@
 import React from 'react'
 import Card from '../components/Card'
+import cardImageBlogs from '../images/card_image_1.png'
+import cardImagePopularTweets from '../images/card_image_2.png'
+import cardImageChallenges from '../images/card_image_3.png'
+import cardImageMixtape from '../images/card_image_4.png'
 
-const ProjectsPage = ({ data }) => {
-  const imagesBaseURL = '../src/images'
-  // TODO: Update repos to include link, desc, imgs and update Cards
+const ProjectsPage = () => {
   const repos = {
     Blogs: {
       link: 'https://github.com/miafrank/blog',
       description: `
-        Tech: React | TypeScript | Tailwind | Flowbite 
+        Tech: React | TypeScript | Tailwind
+
         Description: Repository for this website`,
-      image: `${imagesBaseURL}/card_image_1`
+      image: cardImageBlogs
     },
-    PopularTweets: 'https://github.com/miafrank/popular-tweets-python',
-    CodeChallenges: 'https://github.com/miafrank/code_challenges',
-    Mixtape: 'https://github.com/miafrank/Mixtape'
-  }
+    PopularTweets: {
+      link: 'https://github.com/miafrank/popular-tweets-python',
+      description: `
+        Tech: Python | Kafka | Twitter API
 
-  const repoDescription = {
-    Blogs: '',
-    PopularTweets: 'Tech: Python | Kafka | Twitter API | Confluent',
-    CodeChallenges: 'Leetcode | Hackerrank Code Challenges',
-    Mixtape: 'Tech: Python | Spotify API | Twilio API'
-  }
+        Description: Repository for this website`,
+      image: cardImagePopularTweets
+    },
+    CodeChallenges: {
+      link: 'https://github.com/miafrank/code_challenges',
+      description: `
+        Tech: Leetcode | Hackerrank
 
+        Description: Repository for this website`,
+      image: cardImageChallenges
+    },
+    Mixtape: {
+      link: 'https://github.com/miafrank/mixtape',
+      description: `
+        Tech: Python | Spotify API | Twilio API
+
+        Description: Repository for this website`,
+      image: cardImageMixtape
+    }
+  }
   return (
-        <div class="h-screen">
-            <div className='card-page'>
-            {
-                Object.entries(repos).map(([key, subject], i) => (
-                <dl className="card" key={i}>
-                    <Card repoName={key} repoLink={repos[key]} repoDescription={repoDescription[key]}></Card>
-                    {/* <span className="input-label">key: {i} Name: {subject.name}</span> */}
-                </dl>
-                ))}
-            </div>
-        </div>
+    <div class='pt-5 pb-5 flex flex-col items-center space-y-6 justify-evenly'>
+      {Object.entries(repos).map(([key, _], i) => (
+        <dl key={i}>
+          <Card
+            repoName={key}
+            repoLink={repos[key].link}
+            repoDescription={repos[key].description}
+            repoImg={repos[key].image}
+          ></Card>
+        </dl>
+      ))}
+    </div>
   )
 }
 export default ProjectsPage
