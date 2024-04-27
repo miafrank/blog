efa352effd0160bd6d0e2c247b6081b10dd8a735
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-04-27 14:08:34 -0500

    Update eslint configuration and add .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
-export default [
-  {
-      rules: {
-          "no-unused-vars": "error",
-          "no-undef": "error"
-      }
-  }
-];
+module.exports = {
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    "no-unused-vars": "warn",
+    "no-undef": "warn"
+  },
+};
