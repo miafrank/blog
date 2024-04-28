7172e14d0d09815b4369a4342619177f092ffd8c
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-04-28 14:24:17 -0500

    Update .eslintrc.js configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,22 @@
 module.exports = {
+  plugins: ['prettier', 'eslint-plugin-html'],
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    "no-unused-vars": "warn",
-    "no-undef": "warn"
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn',
+    'prettier/prettier': 'error',
   },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    },
+  },
+  extends: ['plugin:prettier/recommended'],
 };
