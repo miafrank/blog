d1a910b96810f8a8881494929c6a5e90dc082f14
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-08-23 16:35:44 -0500

    Update home page with about me. Update contacts page with contact form.

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { Button, Checkbox, Label, Textarea, TextInput } from "flowbite-react";
 
 const ConnectPage = () => {
-  // Define project urls you want to show
-  // Grid of four projects
-  // Could alternatively use github API
-  return <div class="h-screen"></div>;
+  return (
+    <form className="h-screen justify-center items-center flex max-w-md flex-col gap-4">
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="email2" value="Your email" />
+        </div>
+        <TextInput id="email2" type="email" required shadow />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="password2" value="Your password" />
+        </div>
+        <TextInput id="password2" type="password" required shadow />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="repeat-password" value="Repeat password" />
+        </div>
+        <TextInput id="repeat-password" type="password" required shadow />
+      </div>
+      <div className="flex items-center gap-2"></div>
+      <div className="max-w-md">
+        <div className="mb-2 block">
+          <Label htmlFor="comment" value="Your message" />
+        </div>
+        <Textarea
+          id="comment"
+          placeholder="Leave a comment..."
+          required
+          rows={4}
+        />
+        <Button type="submit">Send Message</Button>
+      </div>
+    </form>
+  );
 };
+
 export default ConnectPage;
