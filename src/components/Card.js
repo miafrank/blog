64d1c34154d22b1f4a944536a8cfc6acd4b219be
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-08-27 13:03:49 -0500

    Update Card component styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,12 @@ import "../index.css";
 
 const Card = ({ repoName, repoLink, repoDescription, repoImg }) => {
   return (
-    <div class="max-w-sm items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="max-w-sm object-scale-down max-h-full m-auto items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a
         href={repoLink}
         class="block max-w-lg p-4 bg-white border border-tiffany-blue rounded-md shadow hover:bg-tiffany-blue dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       >
-        <img class="rounded-t-lg" src={repoImg} />
+        <img class="rounded-t-lg scale-90" src={repoImg} />
       </a>
       <div class="p-5 rounded-md">
         <a href="#">
