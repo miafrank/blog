9d4445a5d4559904fca4101839efd761b2fcf434
Mia Frank <31712634+miafrank@users.noreply.github.com>
2026-03-08 14:43:46 -0500

    Refactor ProjectsPage to improve project description structure and layout

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -6,61 +6,62 @@ import cardImageMixtape from "../images/card_image_4.png";
 import Card from "../components/Card";
 
 const ProjectsPage = () => {
-  const repos = {
+  const projects = {
     Website: {
       name: "Website",
       link: "https://github.com/miafrank/blog",
-      description: `
-        Tech: React | TypeScript | Tailwind
-        Description: Repository for this website`,
-      image: cardImageBlogs,
+      description: {
+        tech: "React | TypeScript | Tailwind",
+        repo: "Repository for this website."
+      },
+      image: cardImageBlogs
     },
     PopularTweets: {
       name: "Popular Tweets",
       link: "https://github.com/miafrank/popular-tweets-python",
-      description: `
-        Tech: Python | Kafka | Twitter API
-        Description: Twitter scraper that ingests tweets based on a search term`,
-      image: cardImagePopularTweets,
+      description: {
+        tech: "Python | Kafka | Twitter API",
+        repo: "Twitter scraper that ingests tweets based on a search term."
+      },
+      image: cardImagePopularTweets
     },
     CodeChallenges: {
       name: "Code Challenges",
       link: "https://github.com/miafrank/code_challenges",
-      description: `
-        Tech: Python
-        Description: Leetcode | Hackerrank`,
-      image: cardImageChallenges,
+      description: {
+        tech: "Python",
+        repo: "A collection of solutions for Leetcode and Hackerrank."
+      },
+      image: cardImageChallenges
     },
     Mixtape: {
       name: "Mixtape",
       link: "https://github.com/miafrank/mixtape",
-      description: `
-        Tech: Python | Spotify API | Twilio API
-        Description: App to text Spotify music to a friend`,
-      image: cardImageMixtape,
-    },
+      description: {
+        tech: "Python | Spotify API | Twilio API",
+        repo: "App to text Spotify music recommendations to a friend."
+      },
+      image: cardImageMixtape
+    }
   };
+
   return (
-    <div
-      class="flex-col flex-auto pt-5 pb-5 space-y-4 items-center"
-      className="projects-page"
-    >
-      {Object.entries(repos).map(
-        (
-          [key, _], // eslint-disable-line no-unused-vars
-          i,
-        ) => (
-          <dl key={i}>
-            <Card
-              repoName={repos[key].name}
-              repoLink={repos[key].link}
-              repoImg={repos[key].image}
-              repoDescription={repos[key].description}
-            ></Card>
-          </dl>
-        ),
-      )}
+    <div className="w-full min-h-screen py-10 px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-5xl mx-auto items-stretch">
+        {Object.entries(projects).map(([key, project]) => (
+          <Card
+            key={key}
+            repoName={project.name}
+            repoLink={project.link}
+            repoImg={project.image}
+            // Passing the sub-keys as individual props
+            techDescription={project.description.tech}
+            repoDescription={project.description.repo}
+          />
+        ))}
+      </div>
     </div>
   );
 };
+
 export default ProjectsPage;
